b79512de7b4df5f4ac811f8d20ba3cdb21aa0e3e
Louis G <louis@lenra.me>
2023-08-29 12:32:29 +0200

    bugged devtool, fix that first

--- example/src/index.js
+++ example/src/index.js
@@ -1,60 +1,80 @@
 
-// lenra-js usage.
+// import { LenraApp } from 'lenra-js';
+const { LenraApp } = require("lenra-js");
 
-// HIGH LEVEL API
-let opts = {
-    appName: "myApp",
-    clientId: "my-client-id",
-    redirectUri: "http://localhost:3001",
-    scope: "app:websocket",
-    isProd: false, // Optional, Default to false
-    wsUri: "wss://myServer.com/socket", // Optional, default to "wss://api.lenra.io/socket" if prod or "wss://api.lenra.io/socket" for dev.
 
-    server: "http://my-server.com", // Optional, default "https://auth.lenra.io" for prod or "http://localhost:4444" for dev
-    tokenEndpoint: "/oauth2/token", // Optional, default "/oauth2/token"
-    authorizationEndpoint: "/oauth2/auth", // Optional, default "/oauth2/auth"
-}
+var button = document.getElementById("button");
 
-let lenraApp = new LenraApp(opts);
-lenraApp.connect().then(() => {
-    let route = lenraApp.route("/foo", (data) => {
-        // Got my data !
-
-        // You probably don't want to call the increment here, but it's for the example.
-        route.callListener(data.increment);
-    });
+let app = new LenraApp({
+    appName: "test",
+    clientId: "foo",
+    redirectUri: "http://127.0.0.1:3030",
+    scope: ["app:websocket"]
 });
 
-
-
-// Create the oauth2 Client
-let oauthOpts = {
-    clientId: "my-client-id",
-    redirectUri: "http://localhost:3001",
-    scope: "app:websocket",
-    isProd: false, // Optional, default false
-    server: "http://my-server.com", // Optional, default "https://auth.lenra.io" for prod or "http://localhost:4444" for dev
-    tokenEndpoint: "/oauth2/token", // Optional, default "/oauth2/token"
-    authorizationEndpoint: "/oauth2/auth", // Optional, default "/oauth2/auth"
-}
-let client = new LenraOAuth2Client(oauthOpts);
-// Authenticate and get token
-let token = await client.authenticate();
-// Save token in localStorage or other secured place.
-// Use token to connect to websocket !
-let socketOpts = {
-    appName: "myApp",
-    token: token,
-    isProd: false, // Default to false
-    wsUri: "wss://myServer.com/socket", // default to "wss://api.lenra.io/socket" if prod or "wss://api.lenra.io/socket" for dev.
-}
-
-let socket = new LenraSocket(socketOpts);
-socket.connect().then(() => {
-    let route = socket.route("/foo", (data) => {
-        // Got my data !
-
-        // You probably don't want to call the increment here, but it's for the example.
-        route.callListener(data.increment);
+console.log("connecting...");
+app.connect().then(() => {
+    console.log("Connected !");
+    app.route("/global", (data) => {
+        console.log(data);
     });
-});
+})
+
+
+// // HIGH LEVEL API
+// let opts = {
+//     appName: "myApp",
+//     clientId: "my-client-id",
+//     redirectUri: "http://localhost:3001",
+//     scope: "app:websocket",
+//     isProd: false, // Optional, Default to false
+//     wsUri: "wss://myServer.com/socket", // Optional, default to "wss://api.lenra.io/socket" if prod or "wss://api.lenra.io/socket" for dev.
+
+//     server: "http://my-server.com", // Optional, default "https://auth.lenra.io" for prod or "http://localhost:4444" for dev
+//     tokenEndpoint: "/oauth2/token", // Optional, default "/oauth2/token"
+//     authorizationEndpoint: "/oauth2/auth", // Optional, default "/oauth2/auth"
+// }
+
+// let lenraApp = new LenraApp(opts);
+// lenraApp.connect().then(() => {
+//     let route = lenraApp.route("/foo", (data) => {
+//         // Got my data !
+
+//         // You probably don't want to call the increment here, but it's for the example.
+//         route.callListener(data.increment);
+//     });
+// });
+
+
+
+// // Create the oauth2 Client
+// let oauthOpts = {
+//     clientId: "my-client-id",
+//     redirectUri: "http://localhost:3001",
+//     scope: "app:websocket",
+//     isProd: false, // Optional, default false
+//     server: "http://my-server.com", // Optional, default "https://auth.lenra.io" for prod or "http://localhost:4444" for dev
+//     tokenEndpoint: "/oauth2/token", // Optional, default "/oauth2/token"
+//     authorizationEndpoint: "/oauth2/auth", // Optional, default "/oauth2/auth"
+// }
+// let client = new LenraOAuth2Client(oauthOpts);
+// // Authenticate and get token
+// let token = await client.authenticate();
+// // Save token in localStorage or other secured place.
+// // Use token to connect to websocket !
+// let socketOpts = {
+//     appName: "myApp",
+//     token: token,
+//     isProd: false, // Default to false
+//     wsUri: "wss://myServer.com/socket", // default to "wss://api.lenra.io/socket" if prod or "wss://api.lenra.io/socket" for dev.
+// }
+
+// let socket = new LenraSocket(socketOpts);
+// socket.connect().then(() => {
+//     let route = socket.route("/foo", (data) => {
+//         // Got my data !
+
+//         // You probably don't want to call the increment here, but it's for the example.
+//         route.callListener(data.increment);
+//     });
+// });
